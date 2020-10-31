75cf04ca0b151b0ba5aaab3ca530cc7b6be41bc8
Mokshith-dev <mokshith.ajalade@gmail.com>
2020-10-31 22:55:22 +0530

    updated code

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,19 @@
 // import using require
+let shapes = require('./shape');
+let Shape = shapes.Shape;
 
+class Circle extends Shape {
+    constructor() {
+        super(color);
+    }
+    calculateArea() {
+
+    }
+}
 // declare class
 
 
 // export class using module.exports
+module.exports = {
+    Circle: Circle
+}
